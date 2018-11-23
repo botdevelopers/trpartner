8229bbaae27bbd32ce6246be9620e9b93cbe0178
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-23 19:57:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
   if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
+  console.log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
     let props = require(`./komutlar/${f}`);
     log(`Yüklenen komut: ${props.help.name}.`);
@@ -77,16 +77,6 @@ client.unload = command => {
     }
   });
 };
-
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
-		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
-		}
-	}
-});
 
 client.elevation = message => {
   if(!message.guild) {
